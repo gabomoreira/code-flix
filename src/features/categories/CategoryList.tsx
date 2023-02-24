7ee6e805cc327280b745ede9a1583ef04c475977
Gabriel Moreira <gsm.br2003@gmail.com>
2023-02-24 12:14:19 -0300

    feat(category): customizing fields in dataGrid category

--- src/features/categories/CategoryList.tsx
+++ src/features/categories/CategoryList.tsx
@@ -1,12 +1,50 @@
-import { Box, Button, Typography } from '@mui/material'
+import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp } from '@mui/x-data-grid';
+import { Box, Button, IconButton, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { useAppSelector } from '../../app/hooks'
 import { selectCategories } from './CategorySlice'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const CategoryList = () => {
     const categories = useAppSelector(selectCategories)
 
-    console.log(categories)
+    const rows: GridRowsProp = !categories ? [{
+        id: 'Nenhum registro',
+
+    }] : categories?.map(category => ({
+        id: category.id,
+        name: category.name,
+        description: category.description,
+        isActive: category.is_active,
+        createdAt: new Date(category.created_at).toLocaleDateString('pt-BR')
+    }))
+
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', flex: 1 },
+        { field: 'name', headerName: 'Name', flex: 1 },
+        { field: 'description', headerName: 'Description', flex: 1 },
+        { field: 'isActive', headerName: 'Active', flex: 1, type: 'boolean', renderCell: renderIsActiveCell },
+        { field: 'createdAt', headerName: 'Created At', flex: 1 },
+        { field: 'actions', headerName: 'Actions', flex: 1, renderCell: renderActionsCell },
+    ];
+
+    function renderIsActiveCell(rowData: GridRenderCellParams) {
+        if (rowData.value === undefined) return ''
+
+        return (
+            <Typography color={rowData.value ? 'primary' : 'secondary'}>
+                {rowData.value ? 'Active' : 'Inactive'}
+            </Typography>
+        )
+    }
+
+    function renderActionsCell(rowData: GridRenderCellParams) {
+        return (
+            <IconButton onClick={() => console.log('deleteRow')} color='secondary' aria-label="delete" size="medium">
+                <DeleteIcon fontSize="inherit" />
+            </IconButton>
+        )
+    }
 
     return (
         <Box>
@@ -22,11 +60,11 @@ export const CategoryList = () => {
                 </Button>
             </Box>
 
-            <Typography variant='h3' component='h1'>List Category</Typography>
-
-            {categories?.map((item) => (
-                <Typography key={item.id}>{item.name}</Typography>
-            ))}
+            <div style={{ height: 300, width: '100%' }}>
+                <DataGrid rows={rows} columns={columns} pageSize={3}
+                    rowsPerPageOptions={[5]}
+                />
+            </div>
         </Box>
     )
 }
