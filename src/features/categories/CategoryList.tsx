549da94b5a55db887c5c24cfd0e2d46b7aeac123
Gabriel Moreira <gsm.br2003@gmail.com>
2023-02-24 00:10:30 -0300

    feat(category): add table data grid

--- src/features/categories/CategoryList.tsx
+++ src/features/categories/CategoryList.tsx
@@ -1,32 +1,46 @@
-import { Box, Button, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
-import { useAppSelector } from '../../app/hooks'
-import { selectCategories } from './CategorySlice'
+import { Link } from 'react-router-dom';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import { Box, Button, Typography } from '@mui/material';
+
+import { useAppSelector } from '../../app/hooks';
+import { selectCategories } from './CategorySlice';
 
 export const CategoryList = () => {
-    const categories = useAppSelector(selectCategories)
+  const categories = useAppSelector(selectCategories);
 
-    console.log(categories)
+  const rows: GridRowsProp = categories!.map((category) => ({
+    id: category.id,
+    name: category.name,
+    description: category.description,
+  }));
 
-    return (
-        <Box>
-            <Box display='flex' justifyContent='flex-end'>
-                <Button
-                    variant='contained'
-                    color='secondary'
-                    component={Link}
-                    to='/category/create'
-                    style={{ marginBottom: '1rem' }}
-                >
-                    New Category
-                </Button>
-            </Box>
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'description', headerName: 'Description', width: 150 },
+  ];
 
-            <Typography variant='h3' component='h1'>List Category</Typography>
+  console.log(rows);
 
-            {categories?.map((item) => (
-                <Typography key={item.id}>{item.name}</Typography>
-            ))}
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Box display="flex" justifyContent="flex-end">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/category/create"
+          style={{ marginBottom: '1rem' }}
+        >
+          New Category
+        </Button>
+      </Box>
+
+      {categories && (
+        <div style={{ height: 300, width: '100%' }}>
+          <DataGrid rows={rows} columns={columns} />
+        </div>
+      )}
+    </Box>
+  );
+};
